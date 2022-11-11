b0ca28156a4b95aa82bc79d3778b72154dfa5792
RVARELAT <114198174+RVARELAT@users.noreply.github.com>
2022-11-10 17:00:07 -0800

    Script Finished

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,33 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class NewStringChecker implements StringChecker {
+  @Override
+  public boolean CheckString(String s) {
+    if (s.contains("H")) {
+      return true;
+    }
+    return false;
+  }
+
+}
 
 public class TestListExamples {
-  // Write your grading tests here!
+
+  @Test 
+	public void filter() {
+        //StringChecker sc = new StringChecker();
+        List<String> result = new ArrayList<>();
+        List<String> testing = new ArrayList<>();
+
+        testing.add("Hello");
+        result.add("Hello");
+        testing.add("Hey");
+        result.add("Hey");
+        testing.add("Hi");
+        result.add("Hi");
+        testing.add("Yes");
+	}
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,42 @@
 # Create your grading script here
 
-set -e
+CPATH = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+#set -e (stop running when ecounters errors)
 
 rm -rf student-submission
+#step 1
 git clone $1 student-submission
 
+#step 2
+if [[-e "ListExamples.java"]]
+    then 
+        echo "File Found"
+    else
+        echo "Wrong file submitted"
+        exit 0
+fi 
+
+#step 3
+cp TestListExamples.java /student-submission
+
+#step 4
+javac -cp $CPATH *.java
+java -cp org.junit.runner.JUNITCORE TestListExamples
+
+$command 2 > file.txt
+if [[$? -eq 0]]
+    then 
+        echo "Compile Succesful"
+    else 
+        echo "Compilation Failed" 
+        exit 0
+fi 
+
+#step 5
+java -cp org.junit.runner.JUNITCORE TestListExamples > file2.txt
+if [[grep "passed" file2.txt]]
+    then
+        "Grade A"
+    else 
+        "Tests have failed"
+fi
