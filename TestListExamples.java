b0495d77c407efef99df93dc9bc229a0f0ac8d60
RVARELAT <114198174+RVARELAT@users.noreply.github.com>
2022-11-20 21:55:05 -0800

    Changes

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,10 +3,11 @@ import org.junit.*;
 import java.util.ArrayList;
 import java.util.List;
 
+//this error is ok for now
 class NewStringChecker implements StringChecker {
-  @Override
-  public boolean CheckString(String s) {
-    if (s.contains("H")) {
+  //@Override
+  public boolean checkString(String h) {
+    if (h.contains("H")) {
       return true;
     }
     return false;
@@ -19,15 +20,70 @@ public class TestListExamples {
   @Test 
 	public void filter() {
         //StringChecker sc = new StringChecker();
-        List<String> result = new ArrayList<>();
+        StringChecker hsc = new NewStringChecker();
         List<String> testing = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
         testing.add("Hello");
         result.add("Hello");
+
         testing.add("Hey");
         result.add("Hey");
+
         testing.add("Hi");
         result.add("Hi");
+
         testing.add("Yes");
+        
+        List<String> filteredList = ListExamples.filter(testing, hsc);
+        assertEquals(result, filteredList);
+	}
+
+  @Test
+  public void merge() {
+    //lists for testing
+    List<String> list1 = new ArrayList<>();
+    List<String> list2 = new ArrayList<>();
+
+    list1.add("Hello");
+    list1.add("Nice");
+    list2.add("To");
+    list2.add("Meet");
+    list2.add("You!");
+
+    //the result we expect
+    List<String> mergedList = new ArrayList<>();
+
+    mergedList.add("Hello");
+    mergedList.add("Nice");
+    mergedList.add("To");
+    mergedList.add("Meet");
+    mergedList.add("You!");
+
+    List<String> actualMergedList = ListExamples.merge(list1, list2);
+    assertEquals(mergedList, actualMergedList);
+  }
+
+  @Test 
+	public void filterComplicated() {
+        //StringChecker sc = new StringChecker();
+        StringChecker hsc = new NewStringChecker();
+        List<String> testing = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+
+        testing.add("saraH");
+        result.add("saraH");
+
+        testing.add("oH");
+        result.add("oH");
+
+        testing.add("otHer");
+        result.add("otHer");
+
+        //dont want lowercase h
+        testing.add("hi");
+        
+        List<String> filteredList = ListExamples.filter(testing, hsc);
+        assertEquals(result, filteredList);
 	}
 }
--- grade.sh
+++ grade.sh
@@ -1,42 +1,57 @@
 # Create your grading script here
-
-CPATH = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 #set -e (stop running when ecounters errors)
 
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+
 rm -rf student-submission
+
+# make student submission directory (credit to lab group partner Jan Kwong who had this great idea of creating a student submission directory to work with)
+mkdir student-submission
 #step 1
 git clone $1 student-submission
 
+cp TestListExamples.java student-submission
+cd student-submission
+
 #step 2
-if [[-e "ListExamples.java"]]
+# space before [[ ]] to avoid error
+if [[ -f "ListExamples.java" ]]
     then 
-        echo "File Found"
+        echo "File Found: +1 point"
     else
-        echo "Wrong file submitted"
+        echo "Wrong file submitted: Grade 0 (Submit right file or check file name)"
         exit 0
 fi 
 
 #step 3
-cp TestListExamples.java /student-submission
+#cp TestListExamples.java /student-submission
 
 #step 4
+#set +e
 javac -cp $CPATH *.java
-java -cp org.junit.runner.JUNITCORE TestListExamples
+#java -cp org.junit.runner.JUNITCORE TestListExamples
 
-$command 2 > file.txt
-if [[$? -eq 0]]
+#$command 2 > file.txt
+
+if [[ $? -eq 0 ]]
     then 
-        echo "Compile Succesful"
+        echo "Compile Succesful: +1 point"
     else 
-        echo "Compilation Failed" 
+        echo "Compilation Failed: 1/5 Grade" 
         exit 0
 fi 
 
 #step 5
-java -cp org.junit.runner.JUNITCORE TestListExamples > file2.txt
-if [[grep "passed" file2.txt]]
+#credit to lab partner Jan Kwong who I borrowed the idea of using $CPATH to make sure we dont get the error of "error: package org.junit does not exist")
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > file2.txt
+
+if [[ $? -eq 0 ]]
     then
-        "Grade A"
+        cat file2.txt
+        echo "Passed Tests: 5/5 A+ Grade :)"
+        exit
     else 
-        "Tests have failed"
+        cat file2.txt
+        echo "Tests have failed: 2/5 Grade"
+        exit
 fi
